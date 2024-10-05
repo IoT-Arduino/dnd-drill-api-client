956de6207bcedfa4ccde1b14fbfaf9054a74dd72
IoT-Arduino <bunkajin@icloud.com>
2024-10-05 20:27:39 +0900

    Clerk認証（Protected route）追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { setupIonicReact } from '@ionic/react'
 import '@ionic/react/css/core.css'
 import '@ionic/react/css/normalize.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
 
 import './App.css'
 import { MainBoard } from './components/MainBoard'
@@ -10,12 +11,27 @@ import { History } from './components/history/History'
 
 setupIonicReact()
 
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <SignedIn>{children}</SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  );
+};
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainBoard />} />
-        <Route path="/history" element={<History />} />
+        <Route path="/" element={<ProtectedRoute>
+              <MainBoard />
+            </ProtectedRoute>} />
+        <Route path="/history" element={<ProtectedRoute>
+              <History />
+            </ProtectedRoute>} />
       </Routes>
     </BrowserRouter>
   )
