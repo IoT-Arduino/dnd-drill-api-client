b68b0ede87df2f9055912960a59b7bc1bde0e347
IoT-Arduino <bunkajin@icloud.com>
2024-10-14 14:38:38 +0900

    fix api base url

--- src/consts/const.ts
+++ src/consts/const.ts
@@ -4,7 +4,7 @@ export const SAVE_DRILLS_LENGTH: number = 8
 
 const isProd = import.meta.env.PROD
 
-export const API_BASE_URL = !isProd ? 'https://drill-api-app.iot-arduino.workers.dev/api' : 'http://localhost:8787/api';
+export const API_BASE_URL = isProd ? 'https://drill-api-app.iot-arduino.workers.dev/api' : 'http://localhost:8787/api';
 // const API_BASE_URL = 'https://drill-api-app.iot-arduino.workers.dev/api'
 
 console.log(isProd)
